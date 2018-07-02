3e5595c63ec444d1104124cd0589bf2a9a978ba7
KingTJB <tylerjbenko@gmail.com>
2018-07-02 15:38:39 -0400

    unkbuvlgc;juvikjvouglvbvub

--- www.poweredbygearup.com/games/DashyBird/script.js
+++ www.poweredbygearup.com/games/DashyBird/script.js
@@ -1,65 +1,119 @@
-/*
-<!DOCTYPE JavaScript Script jsFile Execution>
-<link.css: styles.css>
-<link.baseHtml |>>>Index<<<|
-*/
-
-//menu
-//VARIABLES
-var screenWidth = 1245;
-var screenHeight = 685;
-var level = 'menu'
-var menuShowing = true;
-var spacepressed = false;
-var gameScreen = document.createElement('canvas');
-var gsc = gameScreen.getContext('2d');
-
-//CANVAS PAINTING
-gameScreen.width = screenWidth;
-gameScreen.height = screenHeight;
-gameScreen.style = 'background-image: url(startscreen.png); border: 4px solid black;';
-document.body.appendChild(gameScreen);
-
-//MUSIC
-var menuLoop = document.createElement('audio')
-menuLoop.src = 'DashyBird-menu-loop.m4a'
-menuLoop.volume = '1';
-menuLoop.autoplay = true;
-menuLoop.loop = true;
-document.body.appendChild(menuLoop);
-
-//LEVEL RELATION
-
-//BIRD DRAWNING
-var bird = document.createElement('img');
-bird.src = 'Birdy.png';
-bird.width = "165"
-bird.height = '117'
-bird.style.display = 'none'
-document.body.appendChild(bird)
-
-//Level 1 Bird Calls; grassland; TJB-alert;
-function levelOne() {
-
-	window.addEventListener('keydown', draw)
-
-	menuShowing = false;
-	level = 1;
-	menuLoop.volume = '0';
-	gsc.drawImage(bird, 300, 300, 165, 117);
-	gameScreen.style = 'background-color: skyblue; background-image: none;';
+function getId(id) {
+	var id = document.getElementById(id);
+	return id;
 }
 
-//MENU CONTROLS
-window.addEventListener('keydown', onKeyDown)
-
-function jump(event) {
-	if (Event.keycode === 32 && !menuShowing === true) {
+function run() {
+	var menuloop = document.getElementById('menuloop')
+	menuloop.play();
+	var m1 = document.getElementById('1m');
+	var m2 = getId('2m');
+	var m3 = getId('3m');
+	var m4 = getId('4m');
+	var m5 = getId('5m');
+	var l1ico = document.getElementById('l1ico')
+	var l2ico = document.getElementById('l2ico')
+	var l3ico = document.getElementById('l3ico')
+	var l4ico = document.getElementById('l4ico')
+	var l5ico = document.getElementById('l5ico')
+	var startscreen = document.getElementById('startscreen');
+	var startGamemode = 0;
+	var levelScreenGamemode = 1;
+	var currentGamemode = startGamemode;
+	window.onload = function () { currentGamemode = startGamemode; }
+	var menuloop = document.getElementById('menuloop')
+	var levelGamemodes = {
+		i: 2,
+		ii: 3,
+		iii: 4,
+		iv: 5,
+		v: 6
 	}
-}
 
-function onKeyDown(event) {
-	if (Event.keyCode === 32 && menuShowing === true) {
-		jump()
+	var canvas = {
+		width: 800,
+		height: 600,
+		element: document.getElementById('canvas'),
+		setup: function () {
+			canvas.element.width = canvas.width;
+			canvas.element.height = canvas.height;
+		},
+		draw: function () {
+			var ctx = canvas.element.getContext('2d');
+			ctx.clearRect(0, 0, 800, 600)
+
+			if (currentGamemode === startGamemode) {
+				ctx.drawImage(startscreen, 0, 0, 800, 600)
+			}
+
+			if (currentGamemode === levelScreenGamemode) {
+				ctx.fillStyle = 'orange'
+				ctx.fillRect(0, 0, 800, 600)
+				ctx.strokeStyle = 'blue';
+				ctx.lineWidth = 6
+				ctx.strokeRect(4, 4, 791, 591)
+				ctx.drawImage(l1ico, 50, 25)
+				ctx.drawImage(l2ico, 300, 25)
+				ctx.drawImage(l3ico, 550, 25)
+				ctx.drawImage(l4ico, 50, 150)
+				ctx.drawImage(l5ico, 300, 150)
+			}
+
+		},
+		update: function () {
+		},
+		init: function () {
+			canvas.setup();
+			canvas.update();
+			canvas.draw();
+			window.requestAnimationFrame(canvas.init);
+		}
 	}
+	canvas.element.onclick = function (event) {
+		if (currentGamemode === levelScreenGamemode) {
+			var x = event.clientX;
+			var y = event.clientY;
+
+			if ((y > 25) && (y < 115)) {
+				if (x > 50 && x < 236) {
+					currentGamemode = levelGamemodes.i
+					menuloop.volume = 0;
+					var mi = document.getElementById('1m');
+					mi.play()
+				}
+				if (x > 300 && x < 486) {
+					currentGamemode = levelGamemodes.ii
+					menuloop.volume = 0;
+					var mii = document.getElementById('2m');
+					mii.play()
+				}
+				if (x > 550 && x < 736) {
+					currentGamemode = levelGamemodes.iii
+					menuloop.volume = 0;
+					var miii = document.getElementById('3m');
+					miii.play()
+				}
+			}
+			if ((y > 150) && (y < 240)) {
+				if (x > 50 && x < 236) {
+					currentGamemode = levelGamemodes.iv
+					menuloop.volume = 0;
+					var miv = document.getElementById('4m');
+					miv.play()
+				}
+				if (x > 300 && x < 486) {
+					currentGamemode = levelGamemodes.v
+					menuloop.volume = 0;
+					var mv = document.getElementById('5m');
+					mv.play()
+				}
+			}
+		}
+
+		if (currentGamemode === startGamemode) {
+			currentGamemode = levelScreenGamemode;
+		}
+	}
+	window.addEventListener('load', canvas.init());
+	window.addEventListener('click', onclick());
 }
